015bffa54741c5be99487ed872c1f425d7b95c46
Silas Reinagel <silas.reinagel@carvana.com>
2020-02-25 21:03:35 -0700

    Added Dice Asset

---
+++ src/SpaceSurvivalProto/Assets/Plugins/Alterego Games/Easy Dice Roller Pack/Scrips/LoadOtherLevel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadOtherLevel : MonoBehaviour {
+
+	public void OnLoadLevel() {
+		if(Application.loadedLevel == 0){
+			Application.LoadLevel("DiceExample2");
+		}else{
+			Application.LoadLevel("DiceExample1");
+		}
+	}
+}
