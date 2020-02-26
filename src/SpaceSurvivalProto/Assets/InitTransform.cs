c66427612ce7c91e630ccd987fad9ed47ca47348
Silas Reinagel <silas.reinagel@carvana.com>
2020-02-25 19:11:37 -0700

    Initial Commit

---
+++ src/SpaceSurvivalProto/Assets/InitTransform.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class InitTransform : MonoBehaviour
+{
+    [SerializeField] private Transform initialState;
+    [SerializeField] private GameObject target;
+
+    private void Awake()
+    {
+        target.transform.position = initialState.position;
+        target.transform.rotation = initialState.rotation;
+    }
+}
---
+++ src/SpaceSurvivalProto/Assets/Scripts/PlayerMovementControls.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovementControls : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 20.0f;
+    
+    private Rigidbody body;
+
+    private float xDir;
+    private float zDir;
+    private float diagonalMoveFactor = 0.7f;
+
+    void Awake ()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        xDir = Input.GetAxisRaw("Horizontal");
+        zDir = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        if (xDir != 0 && zDir != 0) // Check for diagonal movement
+        {
+            xDir *= diagonalMoveFactor;
+            zDir *= diagonalMoveFactor;
+        } 
+
+        body.velocity = new Vector3(xDir * moveSpeed, body.velocity.y, zDir * moveSpeed);
+    }
+}
